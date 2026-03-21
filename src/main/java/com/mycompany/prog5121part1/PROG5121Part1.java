8dd0803f1ec1e7439b6dd63ea020baeb9439e6c1
Student <Student@LabVM2047644.0utl3l4run1ejkcmdabu0wb5na.jnbx.internal.cloudapp.net>
2026-03-21 13:34:00 +0200

    added checkUsername function added checkPasswordComplexity function

--- src/main/java/com/mycompany/prog5121part1/PROG5121Part1.java
+++ src/main/java/com/mycompany/prog5121part1/PROG5121Part1.java
@@ -12,18 +12,45 @@ package com.mycompany.prog5121part1;
 public class PROG5121Part1{
 
       public static void main(String[] args) {
-        System.out.println("Testing");
+        String username = "And_l";
+        boolean valid = checkUsername(username);
+        System.out.println(valid);
+        String password = "ppppkppp";
+        valid = checkPasswordCompexity(password);
+        System.out.println(valid);
+                
     }
       
-      
-    public static boolean checkUsername(String Username){
+    //check if user name is valid 
+    public static boolean checkUsername(String username){
   
-        if (Username.length()<5&&Username.contains("_")){
-          System.out.println("Username successfully captured");}
-        else{System.out.println("Username is not correctly formatted");}{
-        return true;
-    }
-    }
+        if (username.length()<=5&&username.contains("_"))
+        {
+            System.out.println("Username successfully captured");
+            return true;
+        }
+        else
+        {
+            System.out.println("Username is not correctly formatted");
+            return false;
+        }
+    }//end of checkUsername()
+    
+    //check if password is valid
+    public static boolean checkPasswordCompexity(String Password)
+    {              
+        
+        if (Password.length()<=8&&Password.matches(".*[^A-Za-z0-9 ].*")&&Password.matches(".*\\d.*")&&Password.matches(".*[A-Z].*"))
+        {
+            System.out.println("You have success");
+            return true;
+        }  
+        else
+        {
+            System.out.println("Incorrect");
+            return false;
+        }
+    }//end of checkPasswordCompexity
 }
         
         
