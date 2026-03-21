fbe0cba619b097d6d0dad1ad5cb6e786b1642bbd
Student <Student@LabVM2047644.0utl3l4run1ejkcmdabu0wb5na.jnbx.internal.cloudapp.net>
2026-03-21 12:02:15 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/prog5121part1/PROG5121Part1.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prog5121part1;
+
+
+/**
+ *
+ * @author Student
+ */
+public class PROG5121Part1{
+
+      public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+      
+      
+    public static boolean checkUsername(String Username){
+  
+        if (Username.length()<5&&Username.contains("_")){
+          System.out.println("Username successfully captured");}
+        else{System.out.println("Username is not correctly formatted");}{
+        return true;
+    }
+    }
+}
+        
+        
+ 
+
+     
+
